f45e533fc7c62d58e1874ac53b848d250551085e
Kinga <kinga.janiak@student.put.poznan.pl>
2025-10-16 14:55:55 +0200

    nowe

--- Laboratorium2.cpp
+++ Laboratorium2.cpp
@@ -2,10 +2,92 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+/*
+	// ZAD.1  
+	
+	po zmianie na i <= 10 wyswietla sie rowniez 10
+
+
+	for (int i = 5; i <= 7; i++) {     
+	cout << i << endl;                
+	}
+*/
+/*
+	for (int i = 5; i < 7; i++) {
+		cout << i << endl;
+	}
+*/
+/*
+	for (int i = 0; i < 10; i+=2) {
+		cout << i << endl;
+	}
+*/
+/*
+
+
+
+
+*/
+/*
+	for (int i = 10; 0 < i  ; i --) {
+		cout << i << endl;
+	}
+*/    
+
+//ZAD.2 
+ /*
+	int fahr, celsius; //zmienne typu int
+	int start, limit, krok; //zmienne typu int
+
+start = 0; //przypisz 0 do start
+limit = 200; //przypisz 200 do limit
+krok = 20; //przypisz 20 do krok
+
+fahr = start; //przypisz wartosc start do zmiennej fahr , czyli fahr =0
+
+while (fahr <= limit) { //wykonuj petle dopoki wartosc fahr jest <=od zmiennej limit
+	celsius = 5 * (fahr - 32) / 9; //oblicz stopnie C i przypisz wynik do celsius
+	cout << fahr << "\t" << celsius << endl; //wypisz zmienne na ekran
+	fahr = fahr + krok; //zwieksz wartosc zmiennej fahr o wartosc zmiennej krok
+}
+
+*/
+
+//ZAD.3
+/*
+	int fahr = 0, celsius; //zmienne typu int
+	int start, limit, krok; //zmienne typu int
+	
+	start = 0; //przypisz 0 do start
+	limit = 200; //przypisz 200 do limit
+	krok = 20; //przypisz 20 do krok
+
+	while (fahr <= limit) {
+	
+		float celsius = (5.0 / 9.0) * (fahr - 32.0);
+		cout << fahr << "\t" << celsius << endl;
+		fahr = fahr + krok;
+	}
+*/
+
+//ZAD.5
+	float fahr, celsius, limit, krok;
+
+	limit = 200; //przypisz 200 do limit
+	krok = 20; //przypisz 20 do krok
+
+
+	for (fahr = 0.0; fahr <= limit; fahr = fahr + krok) //kod 02_16
+		{
+		float celsius = (5.0 / 9.0)*(fahr - 32.0);
+		printf("%3.0f %6.1f \n", fahr, celsius);
+		}
+
+	return 0;
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
